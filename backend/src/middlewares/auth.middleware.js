7ff3cc497f5872bbca60f4b6bf388742e045213a
fazal199 <fazalhussain1441@gmail.com>
2025-05-10 12:51:26 +0530

    add cookie and localstorage support

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -10,8 +10,7 @@ const authMiddleware = tryCatchBlock(async (req, res, next) => {
 
 
 
-    // const token = req?.cookies?.accessToken || req.headers?.authorization?.replace("Bearer", "").trim();
-    const token = req.headers?.authorization?.replace("Bearer", "").trim();
+    const token = req?.cookies?.accessToken || req.headers?.authorization?.replace("Bearer", "").trim();
 
 
     //if token is not given then send error
